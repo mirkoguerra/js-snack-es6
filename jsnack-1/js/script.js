8c39f107f94206a23fd3317885ed3ed38baa47dc
Mirko Guerra <mirkoguerra93@gmail.com>
2020-11-11 15:09:35 +0100

    jsnack-1

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -3,27 +3,27 @@ $(document).ready(function(){
 
   // PARTE A
   // Creare un oggetto palla che abbia le seguenti proprietà. Nome = palla Peso = 10
-  var ball = {
+  let ball = {
     nome: "palla",
     peso: 10
   };
 
   console.log("creazione dell'oggetto ball", ball);
 
-  for (var key in ball) {
-    $(".a").append(key + ": " + ball[key] + "<br>");
+  for (let key in ball) {
+    $(".a").append(`${key}: ${ball[key]}<br>`);
   }
 
   // PARTE B
   // Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
-  var ballWeight = parseInt(prompt("Inserire il peso della palla"));
+  let ballWeight = parseInt(prompt("Inserire il peso della palla"));
 
   ball.peso = ballWeight;
 
   console.log("ball dopo che l'utente ha modificato il peso", ball);
 
-  for (var key in ball) {
-    $(".b").append(key + ": " + ball[key] + "<br>");
+  for (let key in ball) {
+    $(".b").append(`${key}: ${ball[key]}<br>`);
   }
 
 });
