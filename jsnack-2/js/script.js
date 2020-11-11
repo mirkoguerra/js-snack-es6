33011ab3d3b03b002a1647182c05ff0ae8297af0
Mirko Guerra <mirkoguerra93@gmail.com>
2020-11-11 15:15:17 +0100

    jsnack-2

--- jsnack-2/js/script.js
+++ jsnack-2/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   // Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
 
   // creo un array contenente oggetti, i quali sono biciclette da corsa con chiavi nome e peso
-  var biciclette = [
+  let biciclette = [
     {
       nome: "Graziella",
       peso: "15"
@@ -26,10 +26,10 @@ $(document).ready(function(){
   console.log("array di tutte le biciclette", biciclette);
 
   // inizializzo l'array che conterrà la/e bicicletta/e con il peso minore
-  var bicicletteLeggere = [biciclette[0]];
+  let bicicletteLeggere = [biciclette[0]];
 
   // ciclo sull'array che contiene tutte le biciclette
-  for (var i = 1; i < biciclette.length; i++) {
+  for (let i = 1; i < biciclette.length; i++) {
 
     if (biciclette[i].peso == bicicletteLeggere[0].peso) {
       bicicletteLeggere.push(biciclette[i]);
@@ -44,9 +44,11 @@ $(document).ready(function(){
   console.log("array della/e bicicletta/e con il peso minore", bicicletteLeggere);
 
   // stampo a schermo l'array contenente la/e bicicletta/e con il peso minore
-  for (var i = 0; i < bicicletteLeggere.length; i++) {
-    $(".biciclette-leggere").append("<p>" + bicicletteLeggere[i].nome + "</p>");
+  for (let i = 0; i < bicicletteLeggere.length; i++) {
+    $(".biciclette-leggere").append(`<p>${bicicletteLeggere[i].nome}</p>`);
   }
+
   // stampo a schermo il peso minore
-  $(".peso-minore").append("Il peso delle bici con il peso minore è: " + bicicletteLeggere[0].peso);
+  $(".peso-minore").append(`Il peso delle bici con il peso minore è: ${bicicletteLeggere[0].peso}`);
+
 });
